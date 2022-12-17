bb1c94e5639cc5cee5c94b72b8b2ea344def6195
julicastro <castroejulian@gmail.com>
2022-12-17 02:36:37 -0300

    funcionalidades

---
+++ 02 - diseño/app/src/main/java/demo/android/app/MainActivity.java
@@ -0,0 +1,24 @@
+package demo.android.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void miMetodo(View v){
+        Toast.makeText(this, "Holaaa", Toast.LENGTH_LONG).show();
+    }
+
+
+
+
+}
