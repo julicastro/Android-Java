961bfb31f358e09c5b0e35666d9df34000648177
julicastro <castroejulian@gmail.com>
2023-01-17 02:01:18 -0300

    ejercicio02

--- Ejercicios/ejercicio02-design-on-android/app/src/main/java/demo/android/myapplication/MainActivity.java
+++ Ejercicios/ejercicio02-design-on-android/app/src/main/java/demo/android/myapplication/MainActivity.java
@@ -2,13 +2,42 @@ package demo.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private EditText editText;
+    private final String INGRESO = "Ha ingresado correctamente.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.buttonMainActivity);
+        editText = (EditText) findViewById(R.id.editTextTextPersonNameMainActivity);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String editTextString = editText.getText().toString();
+                if (!editTextString.isEmpty()){
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class); // DESDE DONDE ESTAMOS A DONDE VAMOS
+                    intent.putExtra("greetter", INGRESO); // clave - valor
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Por favor escriba su nombre", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
     }
 }
--- Ejercicios/ejercicio02-design-on-android/app/src/main/java/demo/android/myapplication/SecondActivity.java
+++ Ejercicios/ejercicio02-design-on-android/app/src/main/java/demo/android/myapplication/SecondActivity.java
@@ -2,13 +2,96 @@ package demo.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.internal.CheckableGroup;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private Button button;
+    private RadioButton radioButton;
+    private RadioButton radioButton2;
+    private TextView textView;
+    private Integer edad;
+    private String mensajeAEnviar = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Tomar los datos del Intent del otro activity. Vienen en forma de Bundle (es una caja q contiene eso)
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null && bundle.getString("greetter") != null) {
+            String greetter = bundle.getString("greetter");
+            Toast.makeText(SecondActivity.this, greetter, Toast.LENGTH_SHORT).show();
+            textView.setText(greetter);
+        } else {
+            Toast.makeText(SecondActivity.this, "El parametro vino null desde la otra vista xd", Toast.LENGTH_SHORT).show();
+        }
+
+        seekBar = (SeekBar) findViewById(R.id.seekBarSecActivity);
+        button = (Button) findViewById(R.id.buttonSecActivity);
+        radioButton = (RadioButton) findViewById(R.id.radioButtonSecActivity);
+        radioButton2 = (RadioButton) findViewById(R.id.radioButton2SecActivity);
+        textView = (TextView) findViewById(R.id.textViewSecActivity);
+
+        seekBar.setMax(100);
+        seekBar.setProgress(0); // Set initial progress value
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                edad = i;
+                textView.setText(String.valueOf(edad));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (edad < 18 || edad >= 60) {
+                    Toast.makeText(SecondActivity.this, "Debe tener entre 18 y 60 años", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SecondActivity.this, "Edad Correcta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edad >= 18 && edad <= 60) {
+                    if (radioButton.isSelected() || radioButton2.isSelected()) {
+                        if (radioButton.isSelected()) {
+                            mensajeAEnviar = "Hola";
+                        } else if (radioButton2.isSelected()) {
+                            mensajeAEnviar = "Chau";
+                        }
+                        Intent intent = new Intent(SecondActivity.this, ThirdActivity.class); // DESDE DONDE ESTAMOS A DONDE VAMOS
+                        intent.putExtra("edad", mensajeAEnviar + ". Tu edad es: " + edad); // clave - valor
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(SecondActivity.this, "Edad Incorrecta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
