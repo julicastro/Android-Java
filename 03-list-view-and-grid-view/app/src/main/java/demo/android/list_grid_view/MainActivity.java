2945d67fa11c7c7d0c5b4f2c93eec63cf82901ce
julicastro <castroejulian@gmail.com>
2023-01-19 02:08:18 -0300

    listview

---
+++ 03-list-view-and-grid-view/app/src/main/java/demo/android/list_grid_view/MainActivity.java
@@ -0,0 +1,51 @@
+package demo.android.list_grid_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        List<String> names = new ArrayList<>();
+        names.add("Julian");
+        names.add("Emanuel");
+        names.add("Castro");
+        names.add("Messi");
+
+        // le decimos como queremos q muestre esos datos. se le pasa un layout
+        ArrayAdapter <String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked: " + names.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    /*
+        ListView. Lista de vistas q puede ser un boton, boton y textview, etc.
+        Todos los twits x ejemplo son un ListView q tiene texto y botones de like, etc.
+
+    */
+
+}
