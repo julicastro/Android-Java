de3c7d2506d8f3fd3d5105709bcdf6866c0d9bbc
julicastro <castroejulian@gmail.com>
2023-02-10 02:54:52 -0300

    gdx android game

--- 04-card-view-recicle-view/app/src/main/java/demo/android/cardandrecicleview/MainActivity.java
+++ 04-card-view-recicle-view/app/src/main/java/demo/android/cardandrecicleview/MainActivity.java
@@ -3,18 +3,47 @@ package demo.android.cardandrecicleview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.widget.*;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> names;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        names = this.getAllNames();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mLayout = new LinearLayoutManager(this);
+        mAdapter = new MyAdapter(names, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(String name, int position) {
+                Toast.makeText(MainActivity.this, name + " - ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 
+    private List<String> getAllNames() {
+        return new ArrayList<String>() {{
+            add("Alfred");
+            add("Brian");
+            add("Carlos");
+            add("David");
+            add("Esteban");
 
-
-
-
-
+        }};
+    }
 }
